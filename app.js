b75a93547aefe058e74f997671901d6615ecb2be
vr7798 <vr_77982hotmail.com>
2023-06-20 13:48:40 -0400

    tt

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/play', (req, res) => {
-  const videoUrl = 'http://acsb.sh:80/movie/SstHxJjw/imagi>
-  res.render('player', { videoUrl });
+app.get("/play", (req, res) => {
+  const videoUrl =
+    "http://acsb.sh:80/movie/SstHxJjw/imaginationteste/198712.mp4";
+  res.render("player", { videoUrl: videoUrl });
 });
 
 app.listen(PORT, () => {
   console.log("SERVIDOR RODANDO! NA PORTA", PORT);
 });
-
